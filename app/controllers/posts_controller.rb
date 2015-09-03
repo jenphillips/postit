5fb5b38bbf2b12dfe095a333c8356c1af28bfe3d
Jennifer Pierce <jpierce@Jennifers-MacBook-Pro.local>
2015-09-03 09:48:09 -0700

    Beginning to add forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,15 +8,36 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new  # Creates new in-memory post object
   end
 
   def create
+    # binding.pry
+
+    # title = params['title']
+    # url = params['url']
+    # description = params['description']
+    # Post.create({title: title, url: url, description: description})
+
+    @post = Post.new(post_params)
+
+    if @post.save
+      flash[:notice] = "Your post was created."
+      redirect_to posts_path
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private 
+  def post_params
+    params.require(:post).permit(:title, :url, :description, :user)
   end
 
 end
